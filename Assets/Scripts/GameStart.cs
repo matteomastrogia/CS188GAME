606a83320f1b7d32f281aefa823ff58f39a57e1c
Matteo Mastrogiacomo <matteomastrogiacomo134@gmail.com>
2023-04-27 10:24:22 -0400

    Game Start

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
-    public void GameStart() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
